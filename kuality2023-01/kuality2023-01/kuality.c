3808b0df75d97d68b32bfb493ab53fa55af01640
jayboss23 <96344560+jayboss23@users.noreply.github.com>
2023-03-22 06:00:43 +0900

    Update kuality.c

--- kuality2023-01/kuality2023-01/kuality.c
+++ kuality2023-01/kuality2023-01/kuality.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+#pragma warning(disable:4996)
 
-int main() {
-	print("Hello world!\n");
+int main(void)
+{
+	char ch;
+
+	printf("what's your favorite alphabet?");
+	scanf(" %c", &ch);
+	printf(" %c ?? wow me toooooooooo", ch);
+
 }
